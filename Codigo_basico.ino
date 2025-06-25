ffa2b74bd29bf1b81f5b88bd46a6291a3bdf51c4
Diogo Aquino <diogoaquino666@gmail.com>
2025-06-25 17:44:00 -0300

    Update Codigo_basico.ino

--- Codigo_basico.ino
+++ Codigo_basico.ino
@@ -1,16 +1,16 @@
 #include <Servo.h>
 
-Servo miServo;  // Crea un objeto tipo Servo
+Servo miServo;  
 
 void setup() {
-  miServo.attach(9);  // Conecta el servo al pin digital 9
+  miServo.attach(9);  // servo al pin digital 9
 }
 
 void loop() {
   // Gira de 0° a 180°
   for (int angulo = 0; angulo <= 180; angulo++) {
     miServo.write(angulo);  // Mueve a la posición del ángulo
-    delay(15);              // Espera 15 ms para que se mueva
+    delay(15);         
   }
 
   // Vuelve de 180° a 0°
